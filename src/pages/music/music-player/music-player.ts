e6e51612d936140ace66183733444933fae4e59f
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-05-15 16:18:32 +0630

    add music player

---
+++ src/pages/music/music-player/music-player.ts
@@ -0,0 +1,70 @@
+import { Component, Injector, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+import Plyr from 'plyr';
+
+import { BasePage } from '../../base/base';
+import { MusicProvider as Music } from '../../../providers/music/music';
+
+@IonicPage({
+  segment: 'musics/:id',
+  defaultHistory: ['MusicPage']
+})
+@Component({
+  selector: 'page-music-player',
+  templateUrl: 'music-player.html'
+})
+export class MusicPlayerPage extends BasePage {
+  @ViewChild('audio') audio: ElementRef;
+
+  music: Music;
+  player: any;
+
+  constructor(public injector: Injector) {
+    super(injector);
+
+    this.music = new Music();
+    this.music.id = this.navParams.data.id;
+  }
+
+  async ionViewDidLoad() {
+    try {
+      this.showLoadingView('Loading...');
+      await this.music.fetch();
+      this.showContentView();
+      this.player = this.loadAudio(this.getDownloadUrl('music', this.music.file));
+      this.player.play();
+    } catch (error) {
+      if (error.code === 101) {
+        this.showEmptyView();
+      } else {
+        this.showErrorView();
+      }
+    }
+  }
+
+  loadAudio(url: string) {
+    const audio = this.audio.nativeElement as HTMLAudioElement;
+    audio.src = url;
+    return new Plyr(audio, {
+      iconUrl: 'assets/imgs/plyr.svg',
+      blankVideo: 'assets/misc/blank.mp4',
+      controls: [
+        'play-large',
+        // 'restart',
+        // 'rewind',
+        'play',
+        // 'fast-forward',
+        'progress',
+        'current-time',
+        'mute',
+        'volume',
+        'captions',
+        // 'settings',
+        'pip',
+        'airplay',
+        'fullscreen'
+      ]
+    });
+  }
+}
